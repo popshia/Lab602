5d08830116138677028d9fa6616c1bf8d98f8688
junwei9638 <runruncar0744@gmail.com>
2021-04-26 15:48:37 +0800

    new

--- test.py
+++ test.py
@@ -1,65 +1,61 @@
-from tkinter import *
+import tkinter
+import cv2
+import PIL.Image, PIL.ImageTk
+import time
+class App:
+    def __init__(self, window, window_title, video_source=0):
+        self.window = window
+        self.window.title(window_title)
+        self.video_source = "/Users/junweilee/Desktop/Lab602/1.mp4"
+        # open video source (by default this will try to open the computer webcam)
+        self.vid = MyVideoCapture(self.video_source)
+        # Create a canvas that can fit the above video source size
+        self.canvas = tkinter.Canvas(window, width = self.vid.width, height = self.vid.height)
+        self.canvas.pack()
+        # Button that lets the user take a snapshot
+        self.btn_snapshot=tkinter.Button(window, text="Snapshot", width=50, command=self.snapshot)
+        self.btn_snapshot.pack(anchor=tkinter.CENTER, expand=True)
+        # After it is called once, the update method will be automatically called every delay milliseconds
+        self.delay = 15
+        self.update()
+        self.window.mainloop()
+    
+    def snapshot(self):
+        # Get a frame from the video source
+        ret, frame = self.vid.get_frame()
+        if ret:
+            cv2.imwrite("frame-" + time.strftime("%d-%m-%Y-%H-%M-%S") + ".jpg", cv2.cvtColor(frame, cv2.COLOR_RGB2BGR))
+    def update(self):
+        # Get a frame from the video source
+        ret, frame = self.vid.get_frame()
+        if ret:
+            self.photo = PIL.ImageTk.PhotoImage(image = PIL.Image.fromarray(frame))
+            self.canvas.create_image(0, 0, image = self.photo, anchor = tkinter.NW)
+        self.window.after(self.delay, self.update)
 
-def SedanChecked():
-    if sedan_val.get() == 1:
-        print('Sedan on')
-    elif sedan_val.get() == 0:
-        print('Sedan off')
-def BusChecked():
-    if bus_val.get() == 1:
-        print('bus on')
-    elif bus_val.get() == 0:
-        print('bus off')
-def TruckChecked():
-    if truck_val.get() == 1:
-        print('truck on')
-    elif truck_val.get() == 0:
-        print('truck off')
-def TrailerHeadChecked():
-    if trialerHead_val.get() == 1:
-        print('trialerHead on')
-    elif trialerHead_val.get() == 0:
-        print('trialerHead off')
-def CargoChecked():
-    if cargo_val.get() == 1:
-        print('cargo on')
-    elif cargo_val.get() == 0:
-        print('cargo off')
-def MotoChecked():
-    if moto_val.get() == 1:
-        print('moto on')
-    elif moto_val.get() == 0:
-        print('moto off')
-def BikeChecked():
-    if bike_val.get() == 1:
-        print('bike on')
-    elif bike_val.get() == 0:
-        print('bike off' )
-def PedestrainChecked():
-    if pedestrain_val.get() == 1:
-        print('pesdestrain on')
-    elif pedestrain_val.get() == 0:
-        print('pesdestrain off')
-
-sedan_val = IntVar()
-truck_val = IntVar()
-bus_val = IntVar()
-trialerHead_val = IntVar()
-cargo_val = IntVar()
-moto_val = IntVar()
-bike_val = IntVar()
-pedestrain_val = IntVar()
-
-Checkbutton(ws, text="Sedan", variable = sedan_val, onvalue=1, offvalue=0, command=SedanChecked).pack()
-Checkbutton(ws, text="Truck",  variable = truck_val, onvalue=1, offvalue=0, command=TruckChecked).pack()
-Checkbutton(ws, text="Bus", variable = bus_val, onvalue=1, offvalue=0, command=BusChecked).pack()
-Checkbutton(ws, text="Trialer Head", variable = trialerHead_val, onvalue=1, offvalue=0, command=TrailerHeadChecked).pack()
-Checkbutton(ws, text="Cargo", variable = cargo_val, onvalue=1, offvalue=0, command=CargoChecked).pack()
-Checkbutton(ws, text="Moto", variable = moto_val, onvalue=1, offvalue=0, command=MotoChecked).pack()
-Checkbutton(ws, text="Bike", variable = bike_val, onvalue=1, offvalue=0, command=BikeChecked).pack()
-Checkbutton(ws, text="Pedestrian", variable = pedestrain_val, onvalue=1, offvalue=0, command=PedestrainChecked).pack()
-
-ws = Tk()
-ws.title('Interface')
-ws.geometry('200x500')
-ws.mainloop()
+class MyVideoCapture:
+    def __init__(self, video_source=0):
+        # Open the video source
+        self.vid = cv2.VideoCapture(video_source)
+        if not self.vid.isOpened():
+            raise ValueError("Unable to open video source", video_source)
+            # Get video source width and height
+        self.width = self.vid.get(cv2.CAP_PROP_FRAME_WIDTH)
+        self.height = self.vid.get(cv2.CAP_PROP_FRAME_HEIGHT)
+    def get_frame(self):
+        if self.vid.isOpened():
+            ret, frame = self.vid.read()
+            if ret:
+                # Return a boolean success flag and the current frame converted to BGR
+                return (ret, cv2.cvtColor(frame, cv2.COLOR_BGR2RGB))
+            else:
+                return (ret, None)
+        else:
+            return (ret, None)
+        # Release the video source when the object is destroyed
+        def __del__(self):
+            if self.vid.isOpened():
+                self.vid.release()
+    
+    # Create a window and pass it to the Application object
+App(tkinter.Tk(), "Tkinter and OpenCV")
